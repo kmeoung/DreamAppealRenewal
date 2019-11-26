ba0a1f4eb6c60fede7d7404f603c2a97efb67aba
KwonTaeWoong <kmeoung@gmail.com>
2019-11-26 17:47:34 +0900

    Select Step디자인 작업

---
+++ app/src/main/java/com/truevalue/dreamappeal/bean/BeanObjectStepHeader.kt
@@ -0,0 +1,9 @@
+package com.truevalue.dreamappeal.bean
+
+data class BeanObjectStepHeader(
+    var title: String?,
+    var object_idx: Int,
+    var profile_idx: Int,
+    var idx: Int,
+    var position: Int
+)
